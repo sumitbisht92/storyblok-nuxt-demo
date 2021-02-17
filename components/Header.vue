a6eb9d177e9817d558aff787990ff6c473074828
sumitbisht92 <63110104+sumitbisht92@users.noreply.github.com>
2021-02-17 18:48:18 +0530

    commit

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex px-4">
       <div class="w-1/6">Home</div>
-      <div class="w-6/12"><img class=" h-full w-full" :src="blok.image.filename" alt="" /></div>
+      <div class="w-6/12">Image</div>
       <div class="w-2/12">Book</div>
     </div>
 </template>
