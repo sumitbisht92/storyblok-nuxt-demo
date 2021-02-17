05f1e8c23cec08b4ba5dd3848c6907312b377800
sumitbisht92 <63110104+sumitbisht92@users.noreply.github.com>
2021-02-17 20:07:16 +0530

    test1

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ blok.title }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
